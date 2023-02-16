bd4bca6a6cfa8c97b15393ef1002ff602327a31e
Dima <iamdimadem@gmail.com>
2023-02-16 16:22:53 +0000

    fix dynamic h for video

--- components/VideoBackground.jsx
+++ components/VideoBackground.jsx
@@ -1,29 +1,18 @@
-import { useState, useEffect } from "react";
-import ReactPlayer from "react-player/youtube";
+import LiteYouTubeEmbed from "react-lite-youtube-embed";
+import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 
-const videoUrl = "https://www.youtube-nocookie.com/embed/g27irY7PY2M";
+const videoId = "g27irY7PY2M";
 
 export default function VideoBackground() {
-  // check if we are on the client
-  const [isSSR, setIsSSR] = useState(true);
-  useEffect(() => {
-    setIsSSR(false);
-  }, []);
-
   return (
     <>
-      <div className="absolute w-full h-screen overflow-hidden aspect-video transform -z-10">
-        {/* {isSSR ? null : ( */}
-        <iframe
-          width="100%"
-          height="100vh"
-          src="https://www.youtube-nocookie.com/embed/g27irY7PY2M?autoplay=1"
-          frameBorder="0"
-          allowFullScreen
-          allow="autoplay"
-          className="absolute top-0 left-0"
-        ></iframe>
-        {/* )} */}
+      <div className="absolute top-0 w-full h-screen overflow-hidden aspect-video transform -z-10">
+        <LiteYouTubeEmbed
+          id={videoId}
+          title="Vivian Spiral - The Act Kit"
+          noCookie={true}
+          params="controls=0&showinfo=0&autoplay=1&mute=1&loop=1"
+        />
       </div>
     </>
   );
--- pages/videos/index.jsx
+++ pages/videos/index.jsx
@@ -6,15 +6,15 @@ export default function Videos(params) {
     <NavBar>
       <div className="flex flex-col">
         <div className="flex flex-col w-full lg:flex-row">
-          <div className="grid flex-grow h-[330px] aspect-video card  place-items-center m-2">
+          <div className="grid flex-grow h-auto aspect-video card  place-items-center m-2">
             <iframe
               width="100%"
               height="100%"
               src="https://www.youtube.com/embed/o5WGk-DTe88"
               title="YouTube video player"
-              frameborder="0"
+              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
+              allowFullScreen
             ></iframe>
           </div>
           <div className="divider lg:divider-horizontal"></div>
@@ -24,9 +24,9 @@ export default function Videos(params) {
               height="100%"
               src="https://www.youtube.com/embed/MScHgmv_U0E"
               title="YouTube video player"
-              frameborder="0"
+              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
+              allowFullScreen
             ></iframe>
           </div>
         </div>
@@ -38,9 +38,9 @@ export default function Videos(params) {
               height="100%"
               src="https://www.youtube.com/embed/ln3Ihqp7AMg"
               title="YouTube video player"
-              frameborder="0"
+              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowfullscreen
+              allowFullScreen
             ></iframe>
           </div>
           <div className="divider lg:divider-horizontal"></div>
@@ -50,9 +50,9 @@ export default function Videos(params) {
               height="100%"
               src="https://www.youtube.com/embed/qgBPHZI7z9U"
               title="YouTube video player"
-              frameborder="0"
+              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowfullscreen
+              allowFullScreen
             ></iframe>
           </div>
         </div>
