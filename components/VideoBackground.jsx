ac3d7d62f54bbdb310878d821fd2448e98b114c6
Dima <iamdimadem@gmail.com>
2023-02-20 15:13:39 +0000

    final fixes for reindex images

--- components/VideoBackground.jsx
+++ components/VideoBackground.jsx
@@ -1,14 +1,10 @@
 export default function VideoBackground() {
   return (
-    <>
-      {/* <div className="absolute top-0 w-fit min-h-screen overflow-hidden aspect-video -z-10"> */}
-      <iframe
-        src="https://player.vimeo.com/video/799926239?muted=1&autoplay=1&loop=1&autopause=0&title=0&byline=0&portrait=0&background=1"
-        className="bg-video"
-        title="Spiral_Circus_Artist_Demo_Reel"
-        allow="autoplay; fullscreen; picture-in-picture"
-      />
-      {/* </div> */}
-    </>
+    <iframe
+      src="https://player.vimeo.com/video/799926239?muted=1&autoplay=1&loop=1&autopause=0&title=0&byline=0&portrait=0&background=1"
+      className="bg-video"
+      title="Spiral_Circus_Artist_Demo_Reel"
+      allow="autoplay; fullscreen; picture-in-picture"
+    />
   );
 }
