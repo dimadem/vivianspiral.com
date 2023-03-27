5ab1db6a408e3314f3bcc9a6d0a7bd959476cda6
Dima <iamdimadem@gmail.com>
2023-03-27 17:05:00 +0100

    playsInLine Video Background

--- components/VideoBackground.jsx
+++ components/VideoBackground.jsx
@@ -1,6 +1,6 @@
 export default function VideoBackground() {
   return (
-    <video muted autoPlay loop className="bg-video">
+    <video muted autoPlay playsInline loop className="bg-video">
       <source src={require("../public/bg_540_lo-rez.mp4")} type="video/mp4" />
     </video>
   );
