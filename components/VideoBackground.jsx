24516765334046ca00d469d3cc34e9bc8775561a
Dima <iamdimadem@gmail.com>
2023-04-07 10:02:47 +0100

    fix video background autoplay

--- components/VideoBackground.jsx
+++ components/VideoBackground.jsx
@@ -1,6 +1,13 @@
 export default function VideoBackground() {
   return (
-    <video controls={false} muted autoPlay loop className="bg-video">
+    <video
+      controls={false}
+      playsinline
+      autoPlay
+      muted
+      loop
+      className="bg-video"
+    >
       <source src={require("../public/bg_540_lo-rez.mp4")} type="video/mp4" />
     </video>
   );
